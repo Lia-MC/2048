2af5a9464feb67a7e8645aa039b9d102dc0cb9b9
Lia-MC <lia.mc73700@gmail.com>
2025-01-14 23:48:53 -0500

    Testing js portion

--- 2048.js
+++ 2048.js
@@ -8,11 +8,18 @@ window.onload = function() {
 }
 
 function setGame() {
+    // board = [
+    //     [0, 0, 0, 0],
+    //     [0, 0, 0, 0],
+    //     [0, 0, 0, 0],
+    //     [0, 0, 0, 0]
+    // ]
+
     board = [
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]
+        [4, 4, 2, 2],
+        [4, 4, 0, 0],
+        [0, 8, 8, 0],
+        [16, 16, 16, 16]
     ]
 
     for (let r = 0; r < rows; r++) {
@@ -47,20 +54,41 @@ function updateTile(tile, num) {
 document.addEventListener("keyup", (e) => {
     if (e.code == "ArrowLeft") {
         slideLeft();
-    }
-    else if (e.code == "ArrowRight") {
-        slideRight();
-    }
-    else if (e.code == "ArrowUp") {
-        slideUp();
-    }
-    else if (e.code == "ArrowDown") {
-        slideDown();
-    }
+    } 
+    // else if (e.code == "ArrowRight") {
+    //     slideRight();
+    // }
+    // else if (e.code == "ArrowUp") {
+    //     slideUp();
+    // }
+    // else if (e.code == "ArrowDown") {
+    //     slideDown();
+    // }
 }) 
 
-function slide() {
-    //
+function filterZeros(row) {
+    // create a new array, where all but 0s are copied
+    return row.filter(num => num != 0); 
+}
+
+function slide(row) {
+    row = filterZeros(row);
+
+    for (let i = 0; i < row.length - 1; i++) {
+        if (row[i] == row[i+1]) {
+            row[i] *= 2;
+            row[i + 1] = 0;
+            score += row[i];
+        }
+    }
+
+    row = filterZeros(row);
+
+    while (row.length < cols) {
+        row.push(0); // adds 0 to the end of the row if empty
+    }
+
+    return row;
 }
 
 function slideLeft() {
@@ -68,5 +96,11 @@ function slideLeft() {
         let cur_row = board[r];
         cur_row = slide(cur_row);
         board[r] = cur_row;
+
+        for (let c = 0; c < cols; c++) {
+            let tile = document.getElementById(r.toString + "-" + c.toString);
+            let num = board[r][c];
+            updateTile(tile, num);
+        }
     } 
 }
